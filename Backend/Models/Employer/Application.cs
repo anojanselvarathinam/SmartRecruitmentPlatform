c9b0e8710af2e78c331a073f214bf34a10b92063
AbiShayiny <grshayiny2301@gmail.com>
2026-08-08 13:18:34 +0530

    Complete 20%

--- Backend/Models/Employer/Application.cs
+++ Backend/Models/Employer/Application.cs
@@ -2,5 +2,22 @@
 {
     public class Application
     {
+        public int ApplicationId { get; set; }
+
+        public int JobId { get; set; }
+
+        public int JobSeekerId { get; set; }
+
+        public decimal MatchScore { get; set; }
+
+        public string Status { get; set; } = "Pending";
+
+        public DateTime AppliedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation Properties
+        public Job Job { get; set; } = null!;
+
     }
 }
