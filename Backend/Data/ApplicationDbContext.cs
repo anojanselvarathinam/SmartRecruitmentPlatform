bad22b2b39da23dd153ea2d062e0b552ae5efbeb
anojanselvarathinam <anojanselvaraththinam@gmail.com>
2026-08-08 05:33:54 +0530

    Auth 10% complete

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartRecruitmentPlatform.Backend.Models.Authentication;
+
+namespace SmartRecruitmentPlatform.Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Backend/Services/Authentication/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using SmartRecruitmentPlatform.Backend.DTOs.Authentication;
+
+namespace SmartRecruitmentPlatform.Backend.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> RegisterAsync(RegisterDto registerDto);
+
+        Task<string?> LoginAsync(LoginDto loginDto);
+    }
+}
