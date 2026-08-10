e7e796f7b74ce0038549f5cb323ee524badb8568
S.Nirojini <sithamparanathannirojini@gmail.com>
2026-08-10 14:19:50 +0530

    merge into submain

--- Backend/DTOs/JobSeeker/SkillGapDto.cs
+++ Backend/DTOs/JobSeeker/SkillGapDto.cs
@@ -1,6 +1,10 @@
-﻿namespace SmartRecruitmentPlatform.Backend.DTOs.JobSeeker
+﻿namespace SmartRecruitmentPlatform.Backend.DTOs.JobSeeker;
+
+public class SkillGapDto
 {
-    public class SkillGapDto
-    {
-    }
+    public int JobId { get; set; }
+
+    public decimal MatchScore { get; set; }
+
+    public List<string> MissingSkills { get; set; } = new();
 }
--- Backend/Repositories/JobSeeker/Interfaces/IJobSeekerRepository.cs
+++ Backend/Repositories/JobSeeker/Interfaces/IJobSeekerRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace SmartRecruitmentPlatform.Backend.Repositories.JobSeeker.Interfaces
+﻿using SmartRecruitmentPlatform.Backend.Models.JobSeeker;
+
+namespace SmartRecruitmentPlatform.Backend.Repositories.JobSeeker.Interfaces;
+
+public interface IJobSeekerRepository
 {
-    public interface IJobSeekerRepository
-    {
-    }
+    Task<JobSeekerProfile?> GetProfileAsync(int userId);
+
+    Task<JobSeekerProfile?> GetProfileByIdAsync(int profileId);
+
+    Task<JobSeekerProfile> CreateProfileAsync(JobSeekerProfile profile);
+
+    Task<JobSeekerProfile> UpdateProfileAsync(JobSeekerProfile profile);
 }
